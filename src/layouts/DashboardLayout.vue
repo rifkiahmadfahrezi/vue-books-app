20b9c658710b5fbc3c01ae23b6c5d09b7fc20bd4
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-08-29 20:19:39 +0700

    feat: create layout components

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,20 @@
+ <script lang="ts" setup >
+ import Sidebar from '../components/Sidebar.vue';
+ import DashboardNav from '../components/DashboardNav.vue';
+ import { RouterView } from 'vue-router'
+ </script>
+<template>
+   <main class="grid md:grid-cols-[300px_1fr]">
+     <div class="col-span-1">
+      <Sidebar />
+     </div>
+     <div class="">
+      <DashboardNav />
+      <main>
+        <RouterView />
+      </main>
+     </div>
+   </main>
+ </template>
+ 
+ 
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+ <script lang="ts" setup >
+ import NavigationBar from '../components/NavigationBar.vue';
+ import { RouterView } from 'vue-router';
+ </script>
+<template>
+   <div>
+     <NavigationBar />
+     <RouterView />
+   </div>
+ </template>
+ 
+ 
