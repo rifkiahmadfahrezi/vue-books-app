95a3e102eae9bd05b4e57554ec8a74a818eeab1a
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-08-30 13:37:43 +0700

    install vue query

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import App from "./App.vue"
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import router from './routes'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const app = createApp(App)
+app.use(VueQueryPlugin)
 app.use(createPinia().use(piniaPluginPersistedstate))
 app.use(router)
 app.mount('#app')
